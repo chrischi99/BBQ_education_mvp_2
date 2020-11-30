93139a71cedf4ea3307c332afceaed6af2c5eb2a
Ruiwei Xiao <1036659062@qq.com>
2020-11-30 13:34:30 +0800

    add build

---
+++ frontend/build/precache-manifest.575bf185b741e6d88944b9f09abd2dba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752ba5dcc14638e195e128eef664cb32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "db1ae045cf8ae8bbd9aa",
+    "url": "./static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "e7af19ec593dbee42e3e",
+    "url": "./static/css/main.42421d33.chunk.css"
+  },
+  {
+    "revision": "db1ae045cf8ae8bbd9aa",
+    "url": "./static/js/2.91583a75.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.91583a75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7af19ec593dbee42e3e",
+    "url": "./static/js/main.a03b4113.chunk.js"
+  },
+  {
+    "revision": "dbab036f87ca1350243a",
+    "url": "./static/js/runtime-main.851de3a6.js"
+  }
+]);
